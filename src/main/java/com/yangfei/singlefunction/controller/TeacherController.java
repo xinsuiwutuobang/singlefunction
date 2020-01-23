6fd0cc26e71b056f2df4a3171bc2da49bde71ff4
yangfei <fei.yang@joyoudata.com>
2020-01-23 11:56:16 +0800

    test

--- src/main/java/com/yangfei/singlefunction/controller/TeacherController.java
+++ src/main/java/com/yangfei/singlefunction/controller/TeacherController.java
@@ -1,9 +1,17 @@
 package com.yangfei.singlefunction.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yangfei.singlefunction.entity.Teacher;
+import com.yangfei.singlefunction.service.ITeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * <p>
@@ -13,9 +21,38 @@ import org.springframework.stereotype.Controller;
  * @author yangfei
  * @since 2019-05-18
  */
-@Controller
+@RestController
 @RequestMapping("/teacher")
 public class TeacherController {
+    @Autowired
+    private ITeacherService iTeacherService;
+    @RequestMapping("/add")
+    public Object add() {
+        Teacher entity = new Teacher();
+        entity.setTid(new Random().nextLong());
+        entity.setTname(String.valueOf(new Random().nextLong()));
+        boolean save = iTeacherService.save(entity);
+        return save;
+    }
 
+    @RequestMapping("/update")
+    public Object update(Long id) {
+        Teacher entity = new Teacher();
+        entity.setTid(id);
+        entity.setTname(String.valueOf(new Random().nextLong()));
+        boolean update = iTeacherService.updateById(entity);
+        return update;
+    }
+
+    @RequestMapping("/selectById")
+    public Object selectById(Long id) {
+        return iTeacherService.getById(id);
+    }
+
+    @RequestMapping("/list")
+    public Object list() {
+        IPage<Teacher> page = new Page<>(0, 10);
+        return iTeacherService.page(page);
+    }
 }
 
